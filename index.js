665492fd8249b79feac24941d26cd516cf9c101d
Dmytro Les <dles@softserveinc.com>
2016-05-16 15:27:09 +0300

    result

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+module.exports = function (/* functions */) {
 
+    var API = function (chain) {
+        this.chain = chain;
+        this.current = null;
 
-module.exports = function() {
-  //implement me
-};  
+        var self = this;
+
+        this.done = (function (self) {
+            return function (err, val) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                if (self.chain.length > self.current + 1) {
+                    self.current++;
+                    self.chain[self.current].call(self, val);
+                } else if (self.end) {
+                    self.end.call(self, val);
+                }
+            }
+        })(self);
+    };
+
+    API.prototype.start = function (val) {
+        if (this.chain.length > 0) {
+            this.current = 0;
+            this.chain[0].call(this, val);
+        }
+        return this;
+    };
+
+    API.prototype.end = function (end) {
+        this.end = end;
+    };
+
+    return new API(arguments);
+};
